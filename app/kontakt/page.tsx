0d205f48b9ad215864f82a719e27a0de2b63d340
Arne Pedersen <arne.lamo@gmail.com>
2024-06-08 19:57:22 +0200

    build fix.

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
+
 import { ContactForm } from "./Contactform";
 
 export default function Kontakt() {
   return (
     <div>
-      <ContactForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ContactForm />
+      </Suspense>
     </div>
   );
 }
